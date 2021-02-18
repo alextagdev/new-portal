433469fccfbb8494af6def32f23856444f371d6b
loveamusementgeo <76053865+loveamusementgeo@users.noreply.github.com>
2021-02-18 11:32:40 +0200

    Update db.php

--- include/db.php
+++ include/db.php
@@ -2,7 +2,7 @@
 
 define ( 'DB_HOST', 'localhost' );
 define ( 'DB_USER', 'root' );
-define ( 'DB_PASSWORD', 'Alexandru123@' );
+define ( 'DB_PASSWORD', '*****' );
 define ( 'DB_NAME', 'portal' );
 
 $mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD,DB_NAME) or die("Nu s-a conectat la server!");
